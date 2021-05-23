e2aafbaf259e5913ada5bae6923780bc589bdd86
eito2307 <eito2307@gmail.com>
2021-05-22 23:20:00 -0600

    changing name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-hosting-demo'`, () => {
+  it(`should have as title 'eduardoherrera'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-hosting-demo');
+    expect(app.title).toEqual('eduardoherrera');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-hosting-demo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('eduardoherrera app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-hosting-demo';
+  title = 'eduardoherrera';
 }
